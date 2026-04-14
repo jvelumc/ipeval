aaa06537e7c193201dbf2dea283683c621cd617a
Jasper van Egeraat <j.w.a.van_egeraat@lumc.nl>
2026-04-14 11:00:07 +0200

    proposed approach

--- dev/scratchpad.R
+++ dev/scratchpad.R
@@ -19,4 +19,38 @@ data$time <- pmin(data$time_event1, data$time_censor2, data$time_censor3)
 prodlim(Hist(time, status, cens.code = 3) ~ 1, data, reverse = TRUE)
 
 
-# Surv(time, status == 0) ~ 1
+
+
+my_func <- function(data, outcome, cens_formula) {
+
+  # proposed approach:
+  # if user wants 1 censoring mechanism, we assume event = 1, censor = 0.
+  # The user then specifies cens_formula = ~ x1 + x2.
+  # this is combined with outcome on the l.h.s.
+
+  # if user wants more than 1 censoring mechanism, user must specify code for
+  # each event/censoring mechanism.
+  # The user specifies cens_formula then as a list as follows
+  # cens_formula = list(
+  #   Hist(time, status, cens.code = 2) ~ x1,
+  #   Hist(time, status, cens.code = 3) ~ 1
+  # )
+  # and a method for each censoring mechanism i.e. c("cox", "km")
+
+  cens_formula <- make_list_if_not_list(cens_formula)
+
+  if (length(cens_formula) == 1) {
+    updated_formula <- stats::update.formula(
+      old = cens_formula[[1]],
+      new = substitute(outcome ~ ., list(outcome = substitute(outcome)))
+    )
+    return(ipc_weights(data, updated_formula, "KM", 6))
+  } else {
+    lapply(cens_formula, FUN = function(f, data) {
+      # run some adapted version of ipc_weights for each formula
+    })
+    # multiply the weights for every subject
+  }
+}
+
+data$ipcw_onemech <- my_func(data, Surv(time, status == 1), cens_formula = ~ 1)$weights
